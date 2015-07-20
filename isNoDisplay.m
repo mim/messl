80584254d82f6cf9c0a6d89b269597a3436be85e
Michael Mandel <mim@mr-pc.org>
2015-07-20 12:29:49 -0700

    look at DISPLAY environment variable for isNoDisplay

--- isNoDisplay.m
+++ isNoDisplay.m
@@ -3,6 +3,12 @@ function v = isNoDisplay()
 % Return true if matlab was called with the -nodisplay option, false
 % otherwise
 
+[~,d] = system('echo $DISPLAY');
+if ~reMatch(d, ':\d+')
+    v = true;
+    return
+end
+
 % From http://www.mathworks.com/matlabcentral/newsreader/view_thread/136261
 ss = get(0, 'Screensize');
 v = all(ss(3:4) == [1 1]);
